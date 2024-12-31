d814b14fd242f787b4ddab76492359d5580057a5
Deep-Dey1 <deepdey715@gmail.com>
2024-12-31 20:04:48 +0530

    for loop

---
+++ 20_for_loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // Working of the for loop
+    // Syntax : 
+    // for (initialization ; condition check ; incrementation){
+        // code block to be executed if the condition check is true
+    }
+
+    // initialization executed only once at the begining of the loop. 
+    // we can initialize multiple variables in this initilaization.
+    // the scope of  the iitialized variable is valid in the loop block only.
+    // all the variables decleared with the same name as the loop initialization variable    
+    // before in the code got hidden by the loop initialized variable inside the loop block.
+    
+
+
+
+}

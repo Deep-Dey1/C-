a76531610a60eef3b1c4c7a002926339e55c2417
Deep-Dey1 <deepdey715@gmail.com>
2025-01-01 20:02:44 +0530

    for loop added

--- 20_for_loop.cpp
+++ 20_for_loop.cpp
@@ -6,15 +6,27 @@ int main(){
     // Syntax : 
     // for (initialization ; condition check ; incrementation){
         // code block to be executed if the condition check is true
-    }
+    //    }
 
     // initialization executed only once at the begining of the loop. 
     // we can initialize multiple variables in this initilaization.
     // the scope of  the iitialized variable is valid in the loop block only.
     // all the variables decleared with the same name as the loop initialization variable    
     // before in the code got hidden by the loop initialized variable inside the loop block.
+    // Syntax : 
+    // for (int i = 0; i < n ; i++){
+    //      body
+    //  }
+    // for(auto element : arr){
+    //     body
+    // }
 
+    // Write a program to print hello world 5 times :
+    for(int i = 1 ; i <= 5 ; i++){
+        cout << "Hello Wolrd" << endl;
+    }
 
-
+    return 0;
 
 }
+

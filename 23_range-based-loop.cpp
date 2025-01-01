61312667b2d9ec52496a894bce73b6a326f56107
Deep-Dey1 <deepdey715@gmail.com>
2025-01-01 21:17:11 +0530

    done 1st

---
+++ 23_range-based-loop.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v = {1,2,3,4,5};
+    for(int i : v ){
+        cout << i << endl;
+    }
+
+    int arr[] = {10,20,30,40,50};
+    for(int i : arr){
+        cout << i << endl;
+    }
+    return 0;
+}
